d85523099e492c8f36156a253464562e887ab986
Jandir A. Cutabiala <jandir.dev@gmail.com>
2022-03-03 03:06:01 -0300

    chore(appshell): configure commit lint

--- packages/appshell/src/Presentation/Pages/App.tsx
+++ packages/appshell/src/Presentation/Pages/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad
         cupiditate eaque eius eveniet facilis fugit illum libero maxime
         molestias mollitia non obcaecati odit perspiciatis quam, quidem rem sed
-        veniam?
+        veniam? Add values.
       </p>
     </div>
   )
